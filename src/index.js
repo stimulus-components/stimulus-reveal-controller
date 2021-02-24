cd96ea9101da029177ed461b47e46e43b657d4c4
Blayne Farinha <blayne.farinha@gmail.com>
2021-02-23 23:12:01 -0400

    Added transistion support
    You can now have transistion effects when revealing/hiding items by
    adding data attributes to the item telling the controller what classes
    to add to the item for the transistion to happen. Got the information
    about how to do this from the great articles and repos linked below so all
    credit to the authors for their great work.
    
    Sebastian De Deyne
    https://sebastiandedeyne.com/javascript-framework-diet/enter-leave-transitions/
    
    Mike McCall article on dev.to
    https://dev.to/mmccall10/tailwind-enter-leave-transition-effects-with-stimulus-js-5hl7
    
    The el-transistion package by Mike McCall
    https://github.com/mmccall10/el-transition
    
    And a pull request from tomclose on the el-transistion repo to fix an
    issue with original classes being overwritten
    https://github.com/mmccall10/el-transition/pull/4

--- src/index.js
+++ src/index.js
@@ -4,25 +4,138 @@ export default class extends Controller {
   static targets = ['item']
   static classes = ['hidden']
 
+  // Lifecycle Events
   connect () {
     this.class = this.hasHiddenClass ? this.hiddenClass : 'hidden'
+    this.isTransitioning = false
   }
 
+  // Actions
+
   toggle () {
-    this.itemTargets.forEach(item => {
-      item.classList.toggle(this.class)
+    if (this.isTransitioning) {
+      return
+    }
+
+    this.isTransitioning = true
+
+    Promise.all(this.itemTargets.map(item => this.toggleItem(item))).then(() => {
+      this.isTransitioning = false
     })
   }
 
   show () {
-    this.itemTargets.forEach(item => {
-      item.classList.remove(this.class)
+    if (this.isTransitioning) {
+      return
+    }
+
+    this.isTransitioning = true
+
+    Promise.all(this.itemTargets.map(item => this.showItem(item))).then(() => {
+      this.isTransitioning = false
     })
   }
 
   hide () {
-    this.itemTargets.forEach(item => {
-      item.classList.add(this.class)
+    if (this.isTransitioning) {
+      return
+    }
+
+    this.isTransitioning = true
+
+    Promise.all(this.itemTargets.map(item => this.hideItem(item))).then(() => {
+      this.isTransitioning = false
+    })
+  }
+
+  // Private
+
+  async toggleItem (item) {
+    if (item.classList.contains(this.class)) {
+      await this.showItem(item)
+      return
+    }
+
+    await this.hideItem(item)
+  }
+
+  async showItem (item) {
+    if (!item.classList.contains(this.class)) {
+      return
+    }
+
+    item.classList.remove(this.class)
+
+    const data = item.dataset
+
+    if (data.transitionShow && data.transitionShowStart && data.transitionShowEnd) {
+      const enterClassList = data.transitionShow.split(' ')
+      const startClassList = data.transitionShowStart.split(' ')
+      const endClassList = data.transitionShowEnd.split(' ')
+
+      await this.animateTransition(item, enterClassList, startClassList, endClassList)
+    }
+  }
+
+  async hideItem (item) {
+    if (item.classList.contains(this.class)) {
+      return
+    }
+
+    const data = item.dataset
+
+    if (data.transitionHide && data.transitionHideStart && data.transitionHideEnd) {
+      const enterClassList = data.transitionHide.split(' ')
+      const startClassList = data.transitionHideStart.split(' ')
+      const endClassList = data.transitionHideEnd.split(' ')
+
+      await this.animateTransition(item, enterClassList, startClassList, endClassList)
+    }
+
+    item.classList.add(this.class)
+  }
+
+  async animateTransition (item, enterClassList, startClassList, endClassList) {
+    const commonClasses = []
+
+    new Set([...enterClassList, ...startClassList, ...endClassList]).forEach(className => {
+      if (item.classList.contains(className)) {
+        commonClasses.push(className)
+      }
+    })
+
+    item.classList.remove(...commonClasses)
+    item.classList.add(...enterClassList)
+    item.classList.add(...startClassList)
+    await this.nextFrame()
+    item.classList.remove(...startClassList)
+    item.classList.add(...endClassList)
+    await this.afterTransition(item)
+    item.classList.remove(...endClassList)
+    item.classList.remove(...enterClassList)
+    item.classList.add(...commonClasses)
+  }
+
+  nextFrame () {
+    return new Promise(resolve => {
+      requestAnimationFrame(() => {
+        requestAnimationFrame(resolve)
+      })
+    })
+  }
+
+  afterTransition (item) {
+    return new Promise(resolve => {
+      const duration =
+        Number(
+          getComputedStyle(item)
+            .transitionDuration.split(',')[0]
+            .replace('s', '')
+        ) * 1000
+
+      setTimeout(() => {
+        resolve()
+      }, duration)
     })
   }
 }
