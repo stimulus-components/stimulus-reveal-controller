c23d9e4aace39ace0c1a06ebbdfcc851fb7cdfce
Guillaume Briday <guillaumebriday@gmail.com>
2022-12-25 22:36:17 -0500

    Bump to Vite 4 and adding lib name

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,26 @@
-const path = require('path')
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
-module.exports = {
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'stimulus-reveal-controller'
-    },
-    rollupOptions: {
-      external: ['@hotwired/stimulus'],
-      output: {
-        globals: {
-          '@hotwired/stimulus': 'Stimulus'
+export default defineConfig(({ mode }) => {
+  if (mode === 'netlify') {
+    return {}
+  }
+
+  return {
+    build: {
+      lib: {
+        entry: resolve(__dirname, 'src/index.ts'),
+        name: 'StimulusRevealController',
+        fileName: 'stimulus-reveal-controller'
+      },
+      rollupOptions: {
+        external: ['@hotwired/stimulus'],
+        output: {
+          globals: {
+            '@hotwired/stimulus': 'Stimulus'
+          }
         }
       }
     }
   }
-}
+})
